3c474ccb11d1b864137f8b4566a5ee83d51c8d49
Francescosommellaa <franc.sommella@gmail.com>
2024-07-27 19:10:19 +0200

    completato design

--- index.php
+++ index.php
@@ -13,14 +13,60 @@
 
     <!-- css -->
     <link rel="stylesheet" href="CSS/style.css">
-
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
-    <div class="container text-center">
-        <h1>Strong Password Generator</h1>
+<body class="container-generator d-flex ">
+    <div class="text-center">
+        <h1 class="first-h">Strong Password Generator</h1>
+
+        <h2 class="second-h">Genera una password sicura</h2>
+    </div>
+
+    <div class="notifiche">
+        <span>Notifiche per l'utente</span>
+    </div>
+
+    <div class="container-input">
+        <form action="">
+            <div>
+                <div class="d-flex input-area-lunghezza">
+                    <h4 class="first-input-h">Lunghezza password:</h4>
+                    <div class="input-space">
+                        <input type="number" name="pw-lenght" min="8" max="32" step="1">
+                        <div>
+                            <input type="radio" name="ripetizioni" value="si" checked="checked" id="si">
+                            <label for="si">Sì</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="ripetizioni" value="no" id="no">
+                            <label for="no">No</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex input-area-ripetizioni">
+                    <h4>Consenti ripetizioni di uno o più caratteri:</h4>
+                    <div class="input-space">
+                        <div>
+                            <input type="checkbox" name="lettere" value="lettere" id="lettere">
+                            <label for="lettere">Lettere</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" name="numeri" value="numeri" id="numeri">
+                            <label for="numeri">Numeri</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" name="simboli" value="simboli" id="simboli">
+                            <label for="simboli">Simboli</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="reset" class="btn btn-secondary">Annulla</button>
+        </form>
     </div>
 </body>
 
