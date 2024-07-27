612ee50898fc21c9c3f3226121ccc41ce92a0f6a
Francescosommellaa <franc.sommella@gmail.com>
2024-07-27 17:37:00 +0200

    inizializzazione

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,10 +10,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-
+    <div class="container text-center">
+        <h1>Strong Password Generator</h1>
+    </div>
 </body>
 
 </html>
